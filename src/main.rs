3ca768fde359a4c27be8bf3d42caf00756c8e45e
lmills <lmills@srcaus.com.au>
2024-07-11 16:29:17 +1000

    Life!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use slint::{Image, Rgb8Pixel, Rgba8Pixel, SharedPixelBuffer};
+use once_cell::sync::Lazy;
+use slint::{Image, Rgb8Pixel, SharedPixelBuffer, Timer};
+use std::{sync::Mutex, time::Duration};
 
 slint::include_modules!();
 
@@ -16,33 +18,86 @@ impl CAPixel {
     }
 }
 
+static TWOD_CA: Lazy<Mutex<Vec<CAPixel>>> =
+    Lazy::new(|| Mutex::new(vec![CAPixel { state: 0 }; 100 * 100]));
+
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
     const WIDTH: u32 = 100;
     const HEIGHT: u32 = 100;
 
-    ui.set_img(one_dimensional_ca_capixel(WIDTH, HEIGHT, 1));
+    ui.set_oned_img(one_dimensional_ca_capixel(WIDTH, HEIGHT, 1));
+    let mut initial_state = vec![CAPixel { state: 0 }; 100 * 100];
+    {
+        initial_state[5250].state = 1;
+        initial_state[5251].state = 1;
+        initial_state[5351].state = 1;
+        initial_state[5151].state = 1;
+        initial_state[5152].state = 1;
+
+        initial_state[550].state = 1;
+        initial_state[551].state = 1;
+        initial_state[552].state = 1;
+        initial_state[553].state = 1;
+        initial_state[554].state = 1;
+        initial_state[555].state = 1;
+
+        initial_state[650].state = 1;
+        initial_state[651].state = 1;
+        initial_state[652].state = 1;
+        initial_state[653].state = 1;
+        initial_state[654].state = 1;
+        initial_state[655].state = 1;
+
+        initial_state[750].state = 1;
+        initial_state[751].state = 1;
+        initial_state[752].state = 1;
+        initial_state[753].state = 1;
+        initial_state[754].state = 1;
+        initial_state[755].state = 1;
+
+        let mut current_state = TWOD_CA.lock().unwrap();
+        current_state.copy_from_slice(&initial_state);
+    }
+    ui.set_twod_img(create_twod_img(&initial_state, 100 as isize, 100 as isize));
+    // ui.set_twod_img(two_dim_ca(100, 100));
+    let timer = Timer::default();
+    let ui_handle = ui.as_weak();
+    timer.start(
+        slint::TimerMode::Repeated,
+        Duration::from_millis(32),
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_twod_img(two_dim_ca(100, 100));
+        },
+    );
 
     ui.on_submit_clicked({
         let ui_handle = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
-            let rule = match (ui.get_rule().parse::<u8>()) {
+            let rule = match ui.get_rule().parse::<u8>() {
                 Ok(r) => r,
-                Err(e) => {println!("Enter a number: {}", e); 0},
+                Err(e) => {
+                    println!("Enter a number: {}", e);
+                    0
+                }
             };
-            ui.set_img(one_dimensional_ca_capixel(WIDTH, HEIGHT, rule));
+            ui.set_oned_img(one_dimensional_ca_capixel(WIDTH, HEIGHT, rule));
         }
     });
     ui.on_text_accepted({
         let ui_handle = ui.as_weak();
-        move |new_text| {
+        move |_new_text| {
             let ui = ui_handle.unwrap();
-            let rule = match (ui.get_rule().parse::<u8>()) {
+            let rule = match ui.get_rule().parse::<u8>() {
                 Ok(r) => r,
-                Err(e) => {println!("Enter a number: {}", e); 0},
+                Err(e) => {
+                    println!("Enter a number: {}", e);
+                    0
+                }
             };
-            ui.set_img(one_dimensional_ca_capixel(WIDTH, HEIGHT, rule));
+            ui.set_oned_img(one_dimensional_ca_capixel(WIDTH, HEIGHT, rule));
         }
     });
 
@@ -55,6 +110,42 @@ fn main() -> Result<(), slint::PlatformError> {
     });
 
     ui.run()
+}
+
+fn two_dim_ca(width: isize, height: isize) -> Image {
+    let array_len = width * height;
+
+    let mut current_state = TWOD_CA.lock().unwrap();
+    let mut next_state = vec![CAPixel { state: 0 }; array_len as usize];
+
+    for ind in 0..array_len {
+        let neighbour_sum = current_state[wrapped_index(ind - width - 1, array_len)].state
+            + current_state[wrapped_index(ind - width, array_len)].state
+            + current_state[wrapped_index(ind - width + 1, array_len)].state
+            + current_state[wrapped_index(ind - 1, array_len)].state
+            + current_state[wrapped_index(ind + 1, array_len)].state
+            + current_state[wrapped_index(ind + width - 1, array_len)].state
+            + current_state[wrapped_index(ind + width, array_len)].state
+            + current_state[wrapped_index(ind + width + 1, array_len)].state;
+        let cur_state = current_state[ind as usize].state;
+        let lookup_table = [0, 0, cur_state, 1, 0, 0, 0, 0, 0];
+
+        next_state[ind as usize].state = lookup_table[neighbour_sum as usize];
+    }
+    current_state.copy_from_slice(&next_state);
+    return create_twod_img(&next_state, width, height);
+}
+
+fn create_twod_img(pixels: &Vec<CAPixel>, width: isize, height: isize) -> Image {
+    let mut pixel_buffer = SharedPixelBuffer::<Rgb8Pixel>::new(width as u32, height as u32);
+    let pixel_buffer_slice = pixel_buffer.make_mut_slice();
+    pixel_buffer_slice.copy_from_slice(
+        &pixels
+            .iter()
+            .map(|x| x.get_pixel())
+            .collect::<Vec<Rgb8Pixel>>(),
+    );
+    return Image::from_rgb8(pixel_buffer);
 }
 
 fn one_dimensional_ca_capixel(w: u32, h: u32, rule: u8) -> Image {
@@ -104,4 +195,12 @@ fn calc_1d_row_capixel(row: &mut [CAPixel], ruleset: &Vec<u8>) -> Vec<CAPixel> {
 
 fn wrapped_index(index: isize, array_len: isize) -> usize {
     (((index % array_len) + array_len) % array_len) as usize
+    // let step1 = index % array_len;
+    // let step2 = step1 + array_len;
+    // let step3 = step2 % array_len;
+    // let result = step3 as usize;
+
+    // println!("Index:{:#}; Step1: {:#}; Step2:{:#}; Step3:{:#}; Result:{:#}", index, step1, step2, step3, result);
+
+    // return result;
 }
